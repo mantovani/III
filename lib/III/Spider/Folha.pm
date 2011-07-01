d7245dc256888e9a1f37547b762e804e1a051d8d
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2011-07-01 10:50:20 -0300

    spider da Folha adicionado

---
+++ lib/III/Spider/Folha.pm
@@ -0,0 +1,68 @@
+package III::Spider::Folha;
+
+use III::Spider;
+use Moose::Role;
+use HTML::TreeBuilder::XPath;
+use Data::Dumper;
+
+has 'link' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'http://www1.folha.uol.com.br/ciencia/'
+);
+
+has 'spider' => (
+    is      => 'ro',
+    isa     => 'Object',
+    default => sub { III::Spider->new }
+);
+
+has 'source' => ( is => 'ro', isa => 'Str', default => 'Folha' );
+
+sub init {
+    my $self = shift;
+    $self->all_news;
+}
+
+sub all_news {
+    my $self = shift;
+    $self->spider->mechanize->get( $self->link );
+    $self->itens( $self->spider->mechanize->content );
+}
+
+sub itens {
+    my ( $self, $html ) = @_;
+    my $tree  = HTML::TreeBuilder::XPath->new_from_content($html);
+    my @itens = $tree->findnodes('//div[@id="newslist"]//a');
+    foreach my $item (@itens) {
+
+        $self->spider->mechanize->get( $item->attr('href') );
+        $self->parser_news(
+            $self->spider->mechanize->content,
+            {
+                title       => $item->as_text,
+                source_link => $item->attr('href'),
+            }
+        );
+    }
+    $tree->delete;
+}
+
+sub parser_news {
+    my ( $self, $news, $infs ) = @_;
+    my $tree = HTML::TreeBuilder::XPath->new_from_content($news);
+
+    $infs->{author} = $tree->findvalue('//div[@id="articleBy"]');
+    $infs->{category} = 'Ciência';
+    $infs->{source}   = $self->source;
+    $infs->{text}     = $tree->findvalue('//div[@id="articleNew"]/p');
+
+    my $keywords = $tree->findnodes('//meta[@name="keywords"]')->[0];
+    if ($keywords) {
+        $infs->{keywords} = [ split /,/, $keywords->attr('content') ];
+    }
+    $self->spider->store($infs);
+    $tree->delete;
+}
+
+42;
