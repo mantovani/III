3395dff85171807ca2225a8012c7d56765068dab
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2011-07-06 12:31:40 -0300

    novo spider do sol concluido

---
+++ lib/III/Spider/Sol.pm
@@ -0,0 +1,67 @@
+package III::Spider::Sol;
+
+use III::Spider;
+use Moose::Role;
+use HTML::TreeBuilder::XPath;
+use Data::Dumper;
+
+has 'link' => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => 'http://sol.sapo.pt/inicio/Tecnologia/Default.aspx'
+);
+
+has 'spider' => (
+    is      => 'ro',
+    isa     => 'Object',
+    default => sub { III::Spider->new }
+);
+
+has 'source' => ( is => 'ro', isa => 'Str', default => 'Sol' );
+
+sub init {
+    my $self = shift;
+    $self->all_news;
+}
+
+sub all_news {
+    my $self    = shift;
+    my $content = $self->spider->agent->get( $self->link );
+    $self->itens($content);
+}
+
+sub itens {
+    my ( $self, $html ) = @_;
+    my $tree  = HTML::TreeBuilder::XPath->new_from_content($html);
+    my @itens = $tree->findnodes('//div[@class="tit_noticia"]/a');
+    foreach my $item (@itens) {
+        my $content = $self->spider->agent->get( $item->attr('href') );
+        $self->parser_news(
+            $content,
+            {
+                title       => $item->as_text,
+                source_link => 'http://sol.sapo.pt' . $item->attr('href'),
+            }
+        );
+    }
+    $tree->delete;
+}
+
+sub parser_news {
+    my ( $self, $news, $infs ) = @_;
+    my $tree = HTML::TreeBuilder::XPath->new_from_content($news);
+
+    $infs->{author}   = 'Desconhecido';
+    $infs->{category} = 'Tecnologia';
+    $infs->{source}   = $self->source;
+
+    $infs->{text} = $tree->findvalue('//div[@id="NewsSummary"]');
+    my $keywords = $tree->findnodes('//meta[@name="keywords"]')->[0];
+    if ($keywords) {
+        $infs->{keywords} = [ split /,/, $keywords->attr('content') ];
+    }
+    $self->spider->store($infs);
+    $tree->delete;
+}
+
+42;
